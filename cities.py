9b8d52e8203fef830981f6c3117386e59a7282a7
Abhimanyu <abhimanyuchhoker16@gmail.com>
2023-05-28 07:54:28 +0530

    Added cities list to cities.py

--- cities.py
+++ cities.py
@@ -1,1 +1,37 @@
 from classes import City
+import json
+
+cities_list = [
+    "guwahati",
+    "bhubaneshwar",
+    "chennai_central_railway_station",
+    "panaji",
+    "agra",
+    "vadodara",
+    "ludhiana",
+    "electric_company",
+    "patna",
+    "bhopal",
+    "howrah_railway_station",
+    "indore",
+    "nagpur",
+    "kochi",
+    "lucknow",
+    "chandigarh",
+    "jaipur",
+    "new_delhi_railway_station",
+    "pune",
+    "hyderabad",
+    "water_works",
+    "ahmedabad",
+    "kolkata",
+    "chennai",
+    "bengaluru",
+    "chhatrapati_shivaji_terminus",
+    "chance",
+    "delhi",
+    "mumbai"
+    ]
+
+# with open('data.json', 'w') as fp:
+#     json.dump(data, fp)
