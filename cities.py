c3ceef3b2beaf5c01ecaf7f8be400fbf1d697020
Abhimanyu <abhimanyuchhoker16@gmail.com>
2023-05-29 08:24:08 +0530

    Initialized cities as class City

--- cities.py
+++ cities.py
@@ -28,10 +28,49 @@ cities_list = [
     "chennai",
     "bengaluru",
     "chhatrapati_shivaji_terminus",
-    "chance",
     "delhi",
     "mumbai"
     ]
+guwahati = City()
+bhubaneshwar = City()
+chennai_central_railway_station = City()
+panaji = City()
+agra = City()
+vadodara = City()
+ludhiana = City()
+electric_company = City()
+patna = City()
+bhopal = City()
+howrah_railway_station = City()
+indore = City()
+nagpur = City()
+kochi = City()
+lucknow = City()
+chandigarh = City()
+jaipur = City()
+new_delhi_railway_station = City()
+pune = City()
+hyderabad = City()
+water_works = City()
+ahmedabad = City()
+kolkata = City()
+chennai = City()
+bengaluru = City()
+chhatrapati_shivaji_terminus = City()
+delhi = City()
+mumbai = City() 
+
+for city in cities_list:
+    name = input("Name: ").lower()
+    price
+    base_rent
+    owner
+    rent_multiplier
+    color_group
+    house_cost
+    houses
+    mortgage_amount
+    mortgaged
 
 # with open('data.json', 'w') as fp:
 #     json.dump(data, fp)
